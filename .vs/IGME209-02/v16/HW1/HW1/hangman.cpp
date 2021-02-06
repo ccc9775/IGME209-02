35d343db78cbca2a1a20c09f393af8650a9f96ab
Chase Call <ccc9775@rit.edu>
2021-02-06 16:40:47 -0500

    commented the code

--- .vs/IGME209-02/v16/HW1/HW1/hangman.cpp
+++ .vs/IGME209-02/v16/HW1/HW1/hangman.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 
+//each time the user fails a guess, a body part is added to the gallows and printed out
 void showGallows(int guessLimit) {
 
 	if (guessLimit == 1) {
@@ -78,9 +79,11 @@ void showGallows(int guessLimit) {
 	}
 }
 
+//checks through the correct letters of the word, and prints it to the correct array
 void showSolved(char word[], char guesses[], char badLetters[]) {
 
-
+		
+	//if the user guesses the right letter, add it to the array and don't add a body part to the gallows
 		if (guesses[0] == 't') {
 
 			word[0] = 't';
@@ -106,13 +109,15 @@ void showSolved(char word[], char guesses[], char badLetters[]) {
 		word[4] = 'h';
 
 		}
+		//if the user did not guess the correct letter
 		else
 		{
-			
+			//if they have already guessed the letter, let them know and don't add to the gallows 
 			if (strchr(badLetters, guesses[0])) {
 
 				cout << endl << "You have already checked for " << guesses[0] << ", and it is still wrong" << endl;
 			}
+			//if they haven't guessed the letter before, tell them it isn't in the word, and add to the gallows and incorrect letter array
 			else
 			{
 				cout << endl << guesses[0] << " is not in the word" << endl;
@@ -122,6 +127,7 @@ void showSolved(char word[], char guesses[], char badLetters[]) {
 				badLetters[length] = guesses[0];
 			}
 
+			//print out the incorrectly guessed letters
 			cout << "Wrong Letters: " << badLetters << endl << endl;
 		}
 
