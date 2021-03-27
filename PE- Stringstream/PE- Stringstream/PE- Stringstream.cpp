4b3f12554c83f5aaaa9cf1ae67bf2fe785773149
Chase Call <ccc9775@rit.edu>
2021-03-27 15:47:54 -0400

    commented the PE

--- PE- Stringstream/PE- Stringstream/PE- Stringstream.cpp
+++ PE- Stringstream/PE- Stringstream/PE- Stringstream.cpp
@@ -8,41 +8,49 @@
 
 int main()
 {
+	//create starting variables
 	int month = 3;
 	int day = 27;
 	int daysOfMonth[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	char birthday[] = { "Chase Call 4/25" };
 
-
+	//create stream for array, and string
 	std::stringstream ss(birthday);
+	std::string firstName;
 	std::string str;
 
+	//create varaibles for month, day, and slash of the array
 	int birthMonth;
 	int birthDay;
 	char slash;
 
+	//parse through the stringstream
+	ss >> firstName  >> str >> birthMonth >> slash >> birthDay;
 
-	ss >> str  >> str >> birthMonth >> slash >> birthDay;
-
+	//while the current day and month aren't equal to the parsed day and month
 	int daysTillBirth = 0;
 	while (month != birthMonth || day != birthDay) {
 
+		//increase days till equal, and increase current day
 		daysTillBirth++;
 		day++;
 
+		//if the current day is not in the current month, increase the month and set the day to 1
 		if (day > daysOfMonth[month]) {
 
 			day = 1;
 			month++;
 
 		}
+		//if the current month is 13, change it to 1
 		if (month == 13) {
 			month = 1;
 		}
 
 	}
 
-	std::cout << str << ", your birthday is in " << daysTillBirth << " day(s).\n";
+	//print out the days until the user's birthday
+	std::cout << firstName << ", your birthday is in " << daysTillBirth << " day(s).\n";
 
 }
 
